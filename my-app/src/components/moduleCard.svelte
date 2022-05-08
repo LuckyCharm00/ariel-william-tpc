a9f2f13cdc6f5f6d0656342b0527755d0d85028e
Ariel Chandra <88024997+LuckyCharm00@users.noreply.github.com>
2022-05-08 13:40:19 +1000

    revert backl

---
+++ my-app/src/components/moduleCard.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let ModuleCard;
+  import ModuleProgress from "./moduleProgress.svelte";
+</script>
+
+<a class ="h-40 w-40" href="">
+    <img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Random Picture">
+    <div><ModuleProgress moduleProgress={ModuleProgress}/></div>
+</a>
---
+++ my-app/src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script>
+    import ModuleCard from "../components/moduleCard.svelte";
+</script>
+
+<svelte:head>
+    <title>Trial</title>
+</svelte:head>
+
+<h1>Ariel-William TPC</h1>
+
+<div class="h-40 w-40 grid grid-cols-3 gap-3">
+    <ModuleCard moduleCard = {ModuleCard}/>
+    <ModuleCard moduleCard = {ModuleCard}/>
+    <ModuleCard moduleCard = {ModuleCard}/>
+</div>
+
+
