c4c81f2f3fbfd9d695ea91f23f2f942b3b3b6538
wric0003 <wric0003@student.monash.edu>
2022-05-08 22:32:18 +1000

    added circle button

--- my-app/src/routes/index.svelte
+++ my-app/src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script>
     import ModuleCard from "../components/moduleCard.svelte";
+import ModuleCircleButton from "../components/moduleCircleButton.svelte";
 </script>
 
 <svelte:head>
@@ -8,10 +9,13 @@
 
 <h1>Ariel-William TPC</h1>
 
-<div class="grid grid-cols-3 gap-3">
-    <ModuleCard moduleCard = {ModuleCard}/>
-    <ModuleCard moduleCard = {ModuleCard}/>
-    <ModuleCard moduleCard = {ModuleCard}/>
+
+<div class="grid grid-cols-10 gap-3 w-full">
+    <div class="col-span-3"><ModuleCard moduleCard = {ModuleCard}/></div>
+    <div class="col-span-3"><ModuleCard moduleCard = {ModuleCard}/></div>
+    <div class="col-span-3"><ModuleCard moduleCard = {ModuleCard}/></div>
+    <div class="col-span-1 self-center"><ModuleCircleButton moduleCircleButton = {ModuleCircleButton}/></div>
 </div>
 
+    
 
