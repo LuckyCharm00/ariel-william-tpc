2ac96a6a1cb2d6f897ff479e141847ba01e2b4db
wric0003 <wric0003@student.monash.edu>
2022-05-08 21:53:56 +1000

    progress bar update

--- my-app/src/components/moduleProgress.svelte
+++ my-app/src/components/moduleProgress.svelte
@@ -4,6 +4,6 @@
 
 
 <!-- <progress class="progress  w-56 h-5 align-middle bg-base-400" value="70" max="100"></progress> -->
-<div class="w-56 bg-base-400 rounded-full ">
-    <div class="bg-blue-light text-xs font-medium text-white text-center p-0.5 leading-none rounded-full" style="width: 45%"> 45%</div>
+<div class="w-96 h-6 bg-base-300 rounded-md m-3">
+    <div class="h-6 bg-blue-light text-sm font-medium text-white text-center p-1 leading-none rounded-md" style="width: 45%"> 45%</div>
 </div>
