a652ddfe1e7b5faff6889161e6a50ff2e8478d61
Ariel Chandra <88024997+LuckyCharm00@users.noreply.github.com>
2022-05-08 12:25:11 +1000

    Update moduleCard.svelte

--- my-app/src/components/moduleCard.svelte
+++ my-app/src/components/moduleCard.svelte
@@ -2,13 +2,6 @@
   export let ModuleCard;
 </script>
 
-<div class="card w-96 bg-base-100 shadow-xl">
-    <figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" /></figure>
-    <div class="card-body">
-      <h2 class="card-title">Shoes!</h2>
-      <p>If a dog chews shoes whose shoes does he choose?</p>
-      <div class="card-actions justify-end">
-        <button class="btn btn-primary">Buy Now</button>
-      </div>
-    </div>
-</div>
+<a a class ="p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center" href="https://api.lorem.space/image/shoes?w=400&h=225">
+    <img class="h-40 w-40" src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Random Picture">
+</a>
