4c5797d1c0f6740c6a292f0c4e8220c251877188
wric0003 <wric0003@student.monash.edu>
2022-05-08 12:41:46 +1000

    push

--- my-app/src/components/moduleCard.svelte
+++ my-app/src/components/moduleCard.svelte
@@ -1,7 +1,9 @@
 <script>
   export let ModuleCard;
+  import ModuleProgress from "./moduleProgress.svelte";
 </script>
 
-<a a class ="p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center" href="https://api.lorem.space/image/shoes?w=400&h=225">
+<a class ="p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center" href="https://api.lorem.space/image/shoes?w=400&h=225">
     <img class="h-40 w-40" src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Random Picture">
+    <div><ModuleProgress moduleProgress={ModuleProgress}/></div>
 </a>
---
+++ my-app/src/components/moduleProgress.svelte
@@ -0,0 +1,6 @@
+<script>
+    export let ModuleProgress
+</script>
+
+
+<progress class="progress progress-primary w-56" value="70" max="100"></progress>
