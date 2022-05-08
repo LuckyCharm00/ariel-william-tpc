30d4b7b22e9ded99e08735ee444b1861561013cd
Ariel Chandra <88024997+LuckyCharm00@users.noreply.github.com>
2022-05-08 13:09:44 +1000

    restart

--- my-app/src/components/moduleCard.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  export let ModuleCard;
-  import ModuleProgress from "./moduleProgress.svelte";
-</script>
-
-<a class ="p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center" href="https://api.lorem.space/image/shoes?w=400&h=225">
-    <img class="h-40 w-40" src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Random Picture">
-    <div><ModuleProgress moduleProgress={ModuleProgress}/></div>
-</a>
--- my-app/src/components/moduleProgress.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-    export let ModuleProgress
-</script>
-
-
-<progress class="progress progress-primary w-56" value="70" max="100"></progress>
